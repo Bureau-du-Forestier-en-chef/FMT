eda417371589725465921c3ffd464e2b40e82881
Bruno Forest <Bruno.Forest@fec.gouv.qc.ca>
2021-08-25 11:23:09 -0400

    Example to transform a map in vector format to FMTforest and to write the corresponding rasters to a folder

---
+++ Examples/C++/maptoFMTforest.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include "FMTforest.h"
+#include "FMTmodelparser.h"
+#include "FMTareaparser.h"
+#include "FMTversion.h"
+#include "FMTlogger.h"
+
+int main()
+{
+    Logging::FMTlogger().logstamp();
+    #if defined _MSC_VER
+        const std::string modellocation = "../Examples/Models/TWD_land/";
+    #else
+        const std::string modellocation = "Examples/Models/TWD_land/";
+    #endif
+    const std::string	primarylocation = modellocation + "TWD_land.pri";
+    const std::string	maplocation = modellocation + "/Carte/TWD_land.shp";
+    const std::string	writelocation = modellocation + "rasters/TestmaptoFMTforest/";
+    Parser::FMTmodelparser mparser;
+    const std::vector<std::string>scenarios(1, "Spatial");
+    const std::vector<Models::FMTmodel> models = mparser.readproject(primarylocation, scenarios);
+    const std::vector<Core::FMTtheme> themes = models.at(0).getthemes();
+    Parser::FMTareaparser areaparser;
+    areaparser.setdefaultexceptionhandler();
+    Spatial::FMTforest forest = areaparser.vectormaptoFMTforest(maplocation,1420,themes,"AGE","SUPERFICIE",1,0.0001,"",0.0,writelocation,false);
+    return 0;
+}
---
+++ Examples/Python/maptoFMTforest.py
@@ -0,0 +1,28 @@
+import sys,os
+sys.path.append("../../")
+sys.path.append("../../../x64/Release")
+sys.path.append("../../Release")
+print(sys.path)
+from FMT import Models
+from FMT import Parser
+
+if __name__ == "__main__":
+    modelparser = Parser.FMTmodelparser()
+    primarylocation = "../Models/TWD_land/TWD_land.pri"
+    models = modelparser.readproject(primarylocation,["Spatial"])
+    sesmodel = Models.FMTsesmodel(models[0])
+    themes = sesmodel.getthemes()
+    areaparser = Parser.FMTareaparser()
+    areaparser.setdefaultexceptionhandler()
+    forest = areaparser.vectormaptoFMTforest("../Models/TWD_land/Carte/TWD_land.shp",
+                                            380,
+                                            themes,
+                                            "AGE",
+                                            "SUPERFICIE",
+                                            1,
+                                            0.0001,
+                                            "",
+                                            0.0,
+                                            "../Models/TWD_land/rasters/TestmaptoFMTforest/",
+                                            False)
+
