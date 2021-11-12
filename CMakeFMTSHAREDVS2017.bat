c426bc8350f4f03a6e759b9ec065932440c7533b
Guillaume Cyr <Guillaume.Cyr@fec.gouv.qc.ca>
2021-11-12 15:29:30 -0500

    Removed useless .cpp and .hpp

--- CMakeFMTSHAREDVS2017.bat
+++ CMakeFMTSHAREDVS2017.bat
@@ -10,8 +10,8 @@ call %vcvars64loc%
 ::You cannot generate an Rcpp module .dll with VS so dont use -DR_DIR with VS. It will also generates examples
 ::This file if for generating FMT based on shared boost lib for Windows 7, need boost with dynamic libraries
 cmake CMakeLists.txt -B build/debug -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Debug -DMOSEK_DIR="C:/PROGRA~1/Mosek/" -DOSI_DIR="C:/MrnMicro/Applic/Cbc-2.10.2/" -DBOOST_DIR="C:/MrnMicro/Applic/boost_1_71_0_shared/" -DGDAL_DIR="C:/MrnMicro/Applic/gdal-2.3.2/" -DGEOS_DIR="C:/MrnMicro/Applic/geos/" -DPYTHON_DIR="C:/MrnMicro/Applic/Anaconda3/"
-cmake --build build/debug --config Debug
+cmake --build build/debug --config Debug -j 8
 cmake --install build/debug --config Debug
 cmake CMakeLists.txt -B build/release -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DMOSEK_DIR="C:/PROGRA~1/Mosek/" -DOSI_DIR="C:/MrnMicro/Applic/Cbc-2.10.2/" -DBOOST_DIR="C:/MrnMicro/Applic/boost_1_71_0_shared/" -DGDAL_DIR="C:/MrnMicro/Applic/gdal-2.3.2/" -DGEOS_DIR="C:/MrnMicro/Applic/geos/" -DPYTHON_DIR="C:/MrnMicro/Applic/Anaconda3/"
-cmake --build build/release --config Release
+cmake --build build/release --config Release -j 8
 cmake --install build/release --config Release
