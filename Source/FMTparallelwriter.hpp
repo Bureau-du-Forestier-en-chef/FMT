0f335029b11f00f127db01279572281d52a14353
Guillaume Cyr <Guillaume.Cyr@fec.gouv.qc.ca>
2021-11-25 13:41:38 -0500

    Fix for R with GDAL3.0

---
+++ Source/FMTparallelwriter.hpp
@@ -0,0 +1,37 @@
+#ifndef FMTparallelwriter_H_INCLUDED
+#define FMTparallelwriter_H_INCLUDED
+#include "FMTmodelparser.hpp"
+#include <vector>
+#include <string>
+#include <memory>
+#include <boost/thread/recursive_mutex.hpp>
+
+class GDALDataset;
+class OGRLayer;
+
+namespace Models {
+	class FMTmodel;
+}
+
+
+namespace Parallel
+{
+	class FMTparallelwriter : public Parser::FMTmodelparser
+	{
+		std::vector<Core::FMToutput>outputstowrite;
+		GDALDataset* resultsdataset;
+		OGRLayer* resultslayer;
+		mutable boost::recursive_mutex mtx;
+	public:
+		FMTparallelwriter() = default;
+		FMTparallelwriter(const FMTparallelwriter& rhs) = default;
+		FMTparallelwriter& operator = (const FMTparallelwriter& rhs) = default;
+		FMTparallelwriter(const std::string& location,
+			const std::string& driver,
+			const std::vector<Core::FMToutput>& outputs,
+			const Models::FMTmodel& model);
+		void write(const std::unique_ptr<Models::FMTmodel>& modelptr, const int& firstperiod,
+			const int& lastperiod, const size_t& itertion) const;
+	};
+}
+#endif
--- Source/FMTreplanningtask.hpp
+++ Source/FMTreplanningtask.hpp
@@ -1,42 +1,77 @@
 #ifndef FMTreplanningtask_H_INCLUDED
 #define FMTreplanningtask_H_INCLUDED
+
 #include "FMTtask.hpp"
-#include "FMTmodel.hpp"
-#include "FMTmodelparser.hpp"
-#include "FMToutput.hpp"
-#include "FMTconstraint.hpp"
-#include "FMTschedule.hpp"
 #include "FMTactualdevelopment.hpp"
+#include "FMTschedule.hpp"
 #include <vector>
+#include <queue>
 #include <memory>
+#include <string>
+
+
+namespace Models
+{
+	class FMTmodel;
+}
+
+namespace Core
+{
+	class FMToutput;
+}
+
+
 
 /// Namespace for parallel tasking may include multithreading / multiprocessing
 namespace Parallel
 {
+	class FMTparallelwriter;
 	class FMTreplanningtask : public FMTtask
 	{
-		std::shared_ptr<Core::FMTactualdevelopment>basearea;
+		////////////////setbaseinfos()/////////////////
+		///////////////////////////////////////////////
+		std::shared_ptr<FMTparallelwriter>resultswriter;
 		std::shared_ptr<Core::FMTschedule>baseschedule;
-		std::vector<size_t>iterationids;
-		std::unique_ptr<Models::FMTmodel>global;
-		std::unique_ptr<Models::FMTmodel>stochastic;
-		std::unique_ptr<Models::FMTmodel>local;
-		std::shared_ptr<Parser::FMTmodelparser>modelparser;
-		//OGRlayer* results;
-		std::vector<Core::FMToutput>targetoutputs;
+		///////////////////////////////////////////////
+		//////////////Non build models////////////////
+		////////////////////////////////////////
+		///////////////////////////////////////
+		std::shared_ptr<Models::FMTmodel>global;//base area is in here.
+		std::shared_ptr<Models::FMTmodel>stochastic;
+		std::shared_ptr<Models::FMTmodel>local;
+		///////////////////////////////////////
+		//////////////////////////////////////
+		std::queue<size_t>replicateids;
 		std::vector<Core::FMTactualdevelopment>dynamicarea;
 		Core::FMTschedule iterationglobalschedule;
 		std::vector<Core::FMTconstraint>localconstraints;
-		int replanningperiodssize;
-		size_t iteration;
+		int replanningperiods;
+		double globalsolutionweight;
+		std::unique_ptr<Models::FMTmodel>domodelplanning(
+			const std::shared_ptr<Models::FMTmodel>model,
+			const int& replanningperiod,
+			bool getsolutionandlocal = false,
+			bool applyscheduleweight = false);
+		void writeresults(const std::unique_ptr<Models::FMTmodel>& modelptr,bool onlyfirstperiod = false);
+		const size_t getiteration() const;
+	protected:
+		virtual void work();
 	public:
 		FMTreplanningtask() = default;
 		FMTreplanningtask(const FMTreplanningtask& rhs) = default;
 		FMTreplanningtask& operator = (const FMTreplanningtask& rhs) = default;
-		//generatetask()
-		/*std::unique_ptr<FMTtask>clone() const;
-		virtual void join();
-		virtual void work();*/
+		std::unique_ptr<FMTtask>clone() const;
+		//unique constructor!
+		FMTreplanningtask(const Models::FMTmodel& globalm,
+						const Models::FMTmodel& stochasticm,
+						const Models::FMTmodel& localm,
+						const std::vector<Core::FMToutput>& outputs,
+						const std::string& outputlocation,
+						const std::string& gdaldriver,
+						const size_t& replicates,
+						const int& replanningperiodssize,
+						const double& globalwweight);
+		virtual std::vector<std::unique_ptr<FMTtask>>split(const size_t& numberoftasks) const;
 
 	};
 
