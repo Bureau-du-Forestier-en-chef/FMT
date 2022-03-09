d5d940bc4f99bc7863f17ae332aa6228459095cc
Guillaume Cyr <Guillaume.Cyr@fec.gouv.qc.ca>
2022-03-09 08:52:28 -0500

    More argument for getnodes in python

---
+++ Examples/Python/replanning_EX1.bat
@@ -0,0 +1,2 @@
+python Replanner.py -pri ../Models/TWD_land/TWD_land.pri -str Globalex1 -tac Localex1 -sto Globalnofire -out OVOLREC,OSUPREC -fol replanning_EX1 -rep 100 -len 10 -rel 10 -thr 2
+PAUSE
---
+++ Examples/Python/testreplanning.bat
@@ -0,0 +1,2 @@
+python Replanner.py -pri ../Models/TWD_land/TWD_land.pri -str Globalreplanning -tac Localreplanning -sto Globalfire -out OVOLREC,OSUPREC -fol replanning -rep 100 -len 10 -rel 10 -thr 2 -com 5,5 8,10
+PAUSE
