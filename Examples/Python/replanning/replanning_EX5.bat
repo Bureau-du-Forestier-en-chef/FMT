de57fb39cc64db1b92641acf4069c8068d731c81
Guillaume Cyr <Guillaume.Cyr@fec.gouv.qc.ca>
2022-03-11 08:35:44 -0500

    _REPLICATE support 1.._LENGTH constraints

--- Examples/Python/replanning/replanning_EX5.bat
+++ Examples/Python/replanning/replanning_EX5.bat
@@ -1,2 +1,2 @@
-python Replanner.py -pri ../../Models/TWD_land/TWD_land.pri -str Globalex5 -tac Localex1 -sto Globalnofire -out OVOLREC,OSUPREC,OSUPPL,VOLINVENT -fol replanning_EX5 -rep 10 -len 20 -rel 20 -thr 2
+python Replanner.py -pri ../../Models/TWD_land/TWD_land.pri -str Globalex1 -tac Localex5 -sto Globalnofire -out OVOLREC,OSUPREC,OSUPPL,VOLINVENT -fol replanning_EX5 -rep 10 -len 20 -rel 20 -thr 2
 PAUSE
