cf2930aaf40eac9a24d591cf2ceb77fbb392b6e5
Guillaume Cyr <Guillaume.Cyr@fec.gouv.qc.ca>
2022-04-13 16:22:50 -0400

    More comment for FMTactualdev and futur and spec

--- Source/FMTbounds.hpp
+++ Source/FMTbounds.hpp
@@ -330,9 +330,19 @@ class FMTlockbounds : public FMTbounds<int>
     };
 
 class FMTyields;
-
+// DocString: FMTspec
+/**
+FMTspec represent the specifications of a group of FMTdevelopement. You can characterize a
+FMTdevelopement by it's period (FMTperbounds), it's age (FMTagebounds), it's lock (FMTlockbounds) and 
+it's yields (yieldbounds). Bounds are "bounds" lower and upper bounds:
+Example: 1>=age<=10, 5>=period<=20, 0>=lock<=10 and 145<=yield<=700
+*/
 class FMTEXPORT FMTspec
     {
+	// DocString: FMTspec::serialize
+	/**
+	serialize function is for serialization, used to do multiprocessing across multiple cpus (pickle in Pyhton)
+	*/
 	friend class Core::FMTyields;
 	friend class boost::serialization::access;
 	template<class Archive>
@@ -345,31 +355,89 @@ class FMTEXPORT FMTspec
 		ar & BOOST_SERIALIZATION_NVP(yieldbounds);
 	}
 protected:
+	// DocString: FMTperbounds::per
+	///Period bounds so lower >= period <= upper 
     FMTperbounds per;
+	// DocString: FMTperbounds::age
+	///Age bounds so lower >= age <= upper 
     FMTagebounds age;
+	// DocString: FMTlockbounds::lock
+	///Lock bounds so lower >= lock <= upper 
     FMTlockbounds lock;
+	// DocString: FMTlockbounds::yieldnames
+	///The names of each yield in the vector of yieldbounds
 	std::vector<std::string>yieldnames;
+	// DocString: FMTlockbounds::yieldbounds
+	///the yields bounds vector lower>=y1<=upper,lower>=y2<=upper
 	std::vector<FMTyldbounds>yieldbounds;
 public:
+	// DocString: FMTspec()
+	/**
+	Default constructor for FMTspec
+	*/
     FMTspec();
+	// DocString: ~FMTspec()
+	/**
+	Destructor for FMTspec
+	*/
     virtual ~FMTspec()=default;
+	// DocString: FMTspec(const FMTspec&)
+	/**
+	FMTspec copy constructor.
+	*/
     FMTspec(const FMTspec& rhs);
+	// DocString: FMTspec::operator=
+	/**
+	Copy assignement of FMTspec
+	*/
     FMTspec& operator = (const FMTspec& rhs);
+	// DocString: FMTspec::add
+	/**
+	Add yields spec or age, period or lock spec from an other spec.
+	*/
     bool add(const FMTspec& rhs);
+	// DocString: FMTspec::setbounds
+	/**
+	Set period bounds
+	*/
     bool setbounds(const FMTperbounds& bound);
+	// DocString: FMTspec::setbounds
+	/**
+	Set age bounds
+	*/
     bool addbounds(const FMTagebounds& bound);
+	// DocString: FMTspec::setyldbounds
+	/**
+	Set yield bounds
+	*/
     bool addbounds(const FMTyldbounds& bound);
+	// DocString: FMTspec::setlockbounds
+	/**
+	Set lock bounds
+	*/
     bool addbounds(const FMTlockbounds& bound);
+	// DocString: FMTspec::allowwithoutyield
+	/**
+	return true if the given period, age and lock respect the age,period and lock bounds.
+	*/
 	inline bool allowwithoutyield(const int& tperiod, const int& tage, const int& tlock) const
 		{
 		return (per.in(tperiod) &&
 			age.in(tage) &&
 			(lock.empty() || (tlock >= lock.lower)));
 		}
+	// DocString: FMTspec::getyieldbound
+	/**
+	return the reference of a given FMTyldbounds using the name of the FMTyldbounds.
+	*/
 	inline const FMTyldbounds& getyieldbound(const std::string& name) const
 		{
 		return yieldbounds.at(std::distance(yieldnames.begin(), std::find(yieldnames.begin(), yieldnames.end(), name)));
 		}
+	// DocString: FMTspec::allowyields
+	/**
+	Return true if all the yields (values) are within the yield bounds.
+	*/
 	inline bool allowyields(const std::vector<double>& values) const
 	{
 		for (size_t location = 0; location < yieldnames.size(); ++location)
@@ -382,6 +450,10 @@ public:
 
 		return true;
 	}
+	// DocString: FMTspec::allow
+	/**
+	Return true if all age,period and lock are within the bounds and when the yields (values) are within the yield bounds
+	*/
 	inline bool allow(const int& tperiod, const int& tage, const int& tlock, const std::vector<double>& values) const
 		{
 		for (size_t location = 0; location < yieldnames.size(); ++location)
@@ -393,28 +465,91 @@ public:
 		}
 		return (allowwithoutyield(tperiod,tage,tlock));
 		}
-	//std::map<std::string,FMTyldbounds>getyldsbounds() const;
+	// DocString: FMTspec::getylds
+	/**
+	Return the yields names of the yield bounds.
+	*/
 	inline const std::vector<std::string>& getylds() const
 		{
 		return yieldnames;
 		}
+	// DocString: FMTspec::getyldbounds
+	/**
+	Return tthe yields bounds of the spec.
+	*/
 	inline const std::vector<FMTyldbounds>& getyldbounds() const
 		{
 		return yieldbounds;
 		}
+	// DocString: FMTspec::operator std::string
+	/**
+	Returns the string reprensentation of the FMTspec like _age >= lower and _Age <= upper and ....
+	*/
     virtual operator std::string() const;
+	// DocString: FMTspec::operator==
+	/**
+	FMTspec equality operator.
+	*/
 	bool operator == (const FMTspec& rhs) const;
+	// DocString: FMTspec::operator<
+	/**
+	FMTspec less than operator.
+	*/
 	bool operator < (const FMTspec& rhs) const;
+	// DocString: FMTspec::hash
+	/**
+	Return the hashed value of FMTspec.
+	*/
 	size_t hash() const;
+	// DocString: FMTspec::empty
+	/**
+	Return true if all bounds are empty.
+	*/
     bool empty() const;
+	// DocString: FMTspec::emptyage
+	/**
+	Return true if age bound is empty.
+	*/
 	bool emptyage() const;
+	// DocString: FMTspec::emptyylds
+	/**
+	Return true if yields bounds are empty.
+	*/
 	bool emptyylds() const;
+	// DocString: FMTspec::emptyperiod
+	/**
+	Return true if period bounds is empty.
+	*/
 	bool emptyperiod() const;
+	// DocString: FMTspec::emptylock
+	/**
+	Return true if lock bounds is empty.
+	*/
 	bool emptylock() const;
+	// DocString: FMTspec::getageupperbound
+	/**
+	Get the upper bounds of the age bounds.
+	*/
 	int getageupperbound() const;
+	// DocString: FMTspec::getagelowerbound
+	/**
+	Get the lower bound of the age bounds.
+	*/
 	int getagelowerbound() const;
+	// DocString: FMTspec::getperiodupperbound
+	/**
+	Get the upper bound of the period bounds.
+	*/
 	int getperiodupperbound() const;
+	// DocString: FMTspec::getperiodlowerbound
+	/**
+	Get the lower bound of the period bounds.
+	*/
 	int getperiodlowerbound() const;
+	// DocString: FMTspec::issubsetof
+	/**
+	return true if this spec is the subset of the rhs spec.
+	*/
 	bool issubsetof(const FMTspec& rhs) const;
     };
 
