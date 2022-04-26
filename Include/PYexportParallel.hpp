ccffa8228af29c3dfe3a41515f6f23f550f7502f
Guillaume Cyr <Guillaume.Cyr@fec.gouv.qc.ca>
2022-04-26 11:42:42 -0400

    Fix #44

--- Include/PYexportParallel.hpp
+++ Include/PYexportParallel.hpp
@@ -21,6 +21,7 @@ License-Filename: LICENSES/EN/LiLiQ-R11unicode.txt
 
 namespace Python
 {
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(push_back_overloads, push_back, 1, 2)
 
 void exportParallel()
     {
@@ -53,8 +54,9 @@ void exportParallel()
 	bp::class_<Parallel::FMTplanningtask, bp::bases<Parallel::FMTtask>>("FMTplanningtask", "@DocString(FMTplanningtask)")
 		.def(bp::init<Parallel::FMTplanningtask>())
 		.def(bp::init<const int&, const int&, const std::string&, std::string,
-			std::vector<std::string>, Core::FMToutputlevel>(
-				bp::args("Min output period", "Max output period", "Output folder", "GDAL driver name", "GDAL driver creation options", "output level"), "@DocString(FMTplanningtask::FMTplanningtask(...))"));
+			std::vector<std::string>, Core::FMToutputlevel, std::string>(
+				bp::args("Min output period", "Max output period", "Output folder", "GDAL driver name", "GDAL driver creation options", "output level", "primary file localisation"), "@DocString(FMTplanningtask::FMTplanningtask(...))"))
+		.def("push_back", &Parallel::FMTplanningtask::push_back, push_back_overloads(bp::args("model", "schedules"), "@DocString(FMTplanningtask::push_back)"));
 
 	define_pylist<Parallel::FMTplanningtask>();
 
