d85d9bac8d1d4873b43f2f8d88a47fa8896623bb
Guillaume Cyr <Guillaume.Cyr@fec.gouv.qc.ca>
2023-03-08 14:17:21 -0500

    Fix #146

--- Examples/C++/maptoFMTforest.cpp
+++ Examples/C++/maptoFMTforest.cpp
@@ -11,14 +11,23 @@
 #include "FMTtheme.hpp"
 #endif
 
-int main()
+int main(int argc, char* argv[])
 {
 #ifdef FMTWITHGDAL
     Logging::FMTlogger().logstamp();
-    const std::string modellocation =  "../../../../Examples/Models/TWD_land/";
-    const std::string	primarylocation = modellocation + "TWD_land.pri";
-    const std::string	maplocation = modellocation + "/Carte/TWD_land.shp";
-    const std::string	writelocation = "../../tests/maptoFMTforest/";
+    const std::string vals = argv[1];
+    std::vector<std::string>results;
+    boost::split(results, vals, boost::is_any_of("|"));
+    const std::string primarylocation = results.at(0);
+    const std::string scenario = results.at(1);
+    const int side = std::stoi(results.at(2));
+    const std::string	maplocation = argv[2];// modellocation + "/Carte/TWD_land.shp";
+    const std::string	writelocation = argv[3];// "../../tests/maptoFMTforest/";
+    std::string lockfield;
+    if (primarylocation.find("TWD_land")==std::string::npos)
+    {
+        lockfield = "STANLOCK";
+    }
     Parser::FMTmodelparser mparser;
     std::vector<Exception::FMTexc>errors;
     errors.push_back(Exception::FMTexc::FMTmissingyield);
@@ -31,12 +40,12 @@ int main()
     errors.push_back(Exception::FMTexc::FMTsame_transitiontargets);
     errors.push_back(Exception::FMTexc::FMTinvalid_geometry);
     mparser.seterrorstowarnings(errors);
-    const std::vector<std::string>scenarios(1,"ROOT");
+    const std::vector<std::string>scenarios(1, scenario);
     const std::vector<Models::FMTmodel> models = mparser.readproject(primarylocation, scenarios);
     const std::vector<Core::FMTtheme> themes = models.at(0).getthemes();
     Parser::FMTareaparser areaparser;
     //areaparser.setdefaultexceptionhandler();
-    Spatial::FMTforest forest = areaparser.vectormaptoFMTforest(maplocation,1420,themes,"AGE","SUPERFICIE",1,0.0001,"",0.0,writelocation,false);
+    Spatial::FMTforest forest = areaparser.vectormaptoFMTforest(maplocation,side,themes,"AGE","SUPERFICIE",1,0.0001, lockfield,0.0,writelocation,false);
 #endif
 	return 0;
 }
