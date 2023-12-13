054b8a96a562f6b20ff85417afe50f16ee76cf9a
gcyr <Guillaume.Cyr@fec.gouv.qc.ca>
2023-12-13 11:31:05 -0500

    Build conflict fix

--- CMakeFMTMSYS2rcran43.sh
+++ CMakeFMTMSYS2rcran43.sh
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 #Install R
 #Install Rtools that fits with R
 #Install Rcpp on R install.packages("Rcpp")
@@ -10,7 +8,6 @@
 #cd FMT
 #Now compile FMT
 
->>>>>>> tomosek10
 cd /home/FMT
 
 #in debug
@@ -18,24 +15,11 @@ cmake CMakeLists.txt \
  -B build/debug \
  -G "MSYS Makefiles" \
  -DCMAKE_BUILD_TYPE=Debug \
-<<<<<<< HEAD
- -DEXTRALINKERFLAGS="-lharfbuzz -lfreetype -lpoppler" \
- -DPYTHON_DIR="C:/logiciels/Anaconda3/" \
- -DCMAKE_BUILD_TYPE=Release \
- -DPARALLEL_TESTS=8 \
- -DMOSEK_DIR="C:/PROGRA~1/Mosek/10.1/" \
- -DOSI_DIR="D:/rtools42/home/Cbc-2.10.5/" \
- -DBOOST_DIR="/x86_64-w64-mingw32.static.posix/" \
- -DGDAL_DIR="/x86_64-w64-mingw32.static.posix/" \
- -DR_DIR="C:/logiciels/R-4.2.1/" \
- -DGEOS_DIR="/x86_64-w64-mingw32.static.posix/"
-=======
  -DPYTHON_DIR=C:/logiciels/Anaconda3/ \
  -DR_DIR=C:/Logiciels/R-4.3.2/ \
  -DPARALLEL_TESTS=8 \
  -DMOSEK_DIR="C:/PROGRA~1/Mosek/10.1/" \
  -DENV_PREFIX="x86_64-w64-mingw32.static.posix"
->>>>>>> tomosek10
 
 cmake --build build/debug --config Debug -j 8
 cmake --install build/debug --config Debug
@@ -46,19 +30,11 @@ cmake CMakeLists.txt \
  -B build/release \
  -G "MSYS Makefiles" \
  -DPYTHON_DIR=C:/logiciels/Anaconda3/ \
-<<<<<<< HEAD
- -DCMAKE_BUILD_TYPE=Release \
- -DPARALLEL_TESTS=8 \
- -DMOSEK_DIR="C:/PROGRA~1/Mosek/10.1/" \
- -DR_DIR=C:/Logiciels/R-4.3.2/ \
- -DCMAKE_FIND_ROOT_PATH=/x86_64-w64-mingw32.static.posix/
-=======
  -DR_DIR=C:/Logiciels/R-4.3.2/ \
  -DCMAKE_BUILD_TYPE=Release \
  -DPARALLEL_TESTS=8 \
  -DMOSEK_DIR="C:/PROGRA~1/Mosek/10.1/" \
  -DENV_PREFIX="x86_64-w64-mingw32.static.posix"
->>>>>>> tomosek10
 
 
 	
