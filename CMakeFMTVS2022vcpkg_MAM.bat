c370a4175131f893140039ff718c9ae534355c22
Marc-Alexandre Martel <marcalexmartel@gmail.com>
2024-07-29 13:45:15 -0400

    MAM.bat with FMT2

--- CMakeFMTVS2022vcpkg_MAM.bat
+++ CMakeFMTVS2022vcpkg_MAM.bat
@@ -17,13 +17,25 @@ cmake CMakeLists.txt ^
 cmake --build build/debug --config Debug -j 8
 cmake --install build/debug --config Debug
 
-::in release
+::in release 
+::FMT
 cmake CMakeLists.txt ^
 	-B build/release ^
 	-G "Visual Studio 17 2022" ^
 	-DBUILD_TYPE=Release ^
 	-DPARALLEL_TESTS=8 ^
 	-DCMAKE_TOOLCHAIN_FILE=D:\FMT\FMT\vcpkg\scripts\buildsystems\vcpkg.cmake ^
+	-DVCPKG_TARGET_TRIPLET=x64-windows ^
+	-DVCPKG_MANIFEST_MODE=ON ^
+	-DONNXR_DIR=D:/FMT/Dependencies/onnxruntime ^
+	-DMOSEK_DIR="C:/PROGRA~1/Mosek/10.1/"
+::FMT2
+cmake CMakeLists.txt ^
+	-B build/release ^
+	-G "Visual Studio 17 2022" ^
+	-DBUILD_TYPE=Release ^
+	-DPARALLEL_TESTS=8 ^
+	-DCMAKE_TOOLCHAIN_FILE=D:\FMT2\FMT\vcpkg\scripts\buildsystems\vcpkg.cmake ^
 	-DVCPKG_TARGET_TRIPLET=x64-windows ^
 	-DVCPKG_MANIFEST_MODE=ON ^
 	-DONNXR_DIR=D:/FMT/Dependencies/onnxruntime ^
