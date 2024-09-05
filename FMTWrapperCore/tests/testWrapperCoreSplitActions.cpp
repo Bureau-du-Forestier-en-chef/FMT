aaf77da7937fd539a290b47226dcdb73d04f447b
Marc-Alexandre Martel <marcalexmartel@gmail.com>
2024-09-05 14:27:56 -0400

    testWrapperCoreSplitAction test dans csv mais le nombre d'action n'augmente pas

--- FMTWrapperCore/tests/testWrapperCoreSplitActions.cpp
+++ FMTWrapperCore/tests/testWrapperCoreSplitActions.cpp
@@ -9,27 +9,28 @@
 int main(int argc, char* argv[])
 {
 
-	std::string PATHPRI;
+	std::string pathPri;
 	std::vector<std::string> splitted;
 	std::vector<std::string> splittedMask;
 	const std::string OUTPUT_DIRECTORY = "../../tests/testWrapperCoreSplitActions";
 	std::string scenarioName;
 	if (argc < 2)
 	{
-		std::string PATHPRI = "D:/Inputs_Prototype/Prototype_Dec2023_MethodeTBE/PC_7002071_UA08152_FINAL.pri";
-		splitted = { "" };
-		splittedMask = { "" };
-		scenarioName = "30_ReglProv_avsp";
+		pathPri = "T:/Donnees/02_Courant/07_Outil_moyen_methode/01_Entretien_developpement/09_FMT/Modeles_test/Prototype_Dec2023/PC_7002071_UA08152_FINAL.pri";
+		splitted = { "AEC", "ACT" };
+		splittedMask = { "? ? ? ? ? ? ? ? ? ? ? ? ? UTA11 ? ? ? ? ? ?", "? ? ? ? ? ? ? ? ? ? ? ? ? !UTA11 ? ? ? ? ? ?" };
+		scenarioName = "01_Regl_prov_apsp_1_6";
 		
 	}
-	else {
+	else 
+	{
 		std::vector<std::string> result;
 		boost::split(result, argv[1], boost::is_any_of("|"));
-		PATHPRI = result.at(0);
+		pathPri = result.at(0);
 		scenarioName = result.at(1);
 		boost::split(splitted, argv[2], boost::is_any_of("|"));
 		boost::split(splittedMask, argv[3], boost::is_any_of("|"));
-
+	}
 	Parser::FMTmodelparser ModelParser;
 
 
@@ -50,7 +51,17 @@ int main(int argc, char* argv[])
 	ModelParser.seterrorstowarnings(errors);
 
 
-	const std::vector<Models::FMTmodel> MODELS = ModelParser.readproject(PATHPRI, { scenarioName });
-	const Models::FMTmodel SPLITTED_MODEL = FMTWrapperCore::Transformation::splitActions(MODELS.at(0), PATHPRI, splitted, SPLITTED_MASK, OUTPUT_DIRECTORY, scenarioName);
+	const std::vector<Models::FMTmodel> MODELS = ModelParser.readproject(pathPri, { scenarioName });
+	const Models::FMTmodel SPLITTED_MODEL = FMTWrapperCore::Transformation::splitActions(MODELS.at(0), pathPri, splitted, splittedMask, OUTPUT_DIRECTORY, scenarioName);
+	
+	// On vérifie si on a plus d'actions dans le nouveau model
+	int splittedCount = SPLITTED_MODEL.getactions().size();
+	int startCount = MODELS.at(0).getactions().size();
+	std::cout << "startCount: " << startCount << std::endl;
+	std::cout << "splittedCount: " << splittedCount << std::endl;
+	if (SPLITTED_MODEL.getactions().size() <= MODELS.at(0).getactions().size())
+	{
+		throw Exception::FMTexc::FMTinvalid_action;
+	}
 	return 0;
 }
