532dabab1540fee04bb222bbaf4157fd9bc8adda
bouto3 <tommy.bouchard@mrnf.gouv.qc.ca>
2026-01-27 13:04:14 -0500

    FMTExcel - Suppression des packages.

--- Excel/FMTExcel/packages/NetOffice.Excel.1.7.4.11/tools/install.ps1
+++
@@ -1,7 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$ref = $project.Object.References.Item("ExcelApi")
-if ($ref -and $ref.EmbedInteropTypes)
-{
-    $ref.EmbedInteropTypes = $false
-}
