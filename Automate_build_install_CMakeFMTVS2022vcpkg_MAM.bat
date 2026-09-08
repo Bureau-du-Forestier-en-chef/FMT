a0352cc6da9c6b263fedaa6bede3258ef2db8ab3
Marc-Alex <marc-alexandre.martel@fec.gouv.qc.ca>
2026-09-08 14:54:33 -0400

    Ajout CMakePresets.json pour les setup build automatiquement reconnu. Ajout .idea dans le gitignore et suppression des.bat à MAM non utilisé

--- Automate_build_install_CMakeFMTVS2022vcpkg_MAM.bat
+++
@@ -1,9 +0,0 @@
-
-call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
-	
-cmake --build build/release --config Release
-
-cmake --install build/release --config Release
-
-pause
-
